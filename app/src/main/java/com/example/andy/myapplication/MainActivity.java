16a93c6d33427b199eca9fc7f69abecb4623de83
Alexandre Merasli <alexandre.merasli@outlook.fr>
2020-07-02 10:23:07 +0200

    can speak as long as we decide

--- app/src/main/java/com/example/andy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andy/myapplication/MainActivity.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,16 +29,20 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private final int REQ_CODE = 100;
     EditText textWidget;
-    Button sendButton;
-    Button deleteText;
+    Button endRecording,sendButton,deleteText;
     RadioButton Messenger,Whatsapp,Mail,Google;
     RadioButton French,English,Spanish;
+    SpeechRecognizer mSpeechRecognizer;
+    Intent mSpeechRecognizerIntent;
+    boolean stopListering = false;
+    String textToSend = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textWidget = findViewById(R.id.text);
+        endRecording = findViewById(R.id.endRecording);
         sendButton = findViewById(R.id.sendMessage);
         deleteText = findViewById(R.id.deleteMessage);
         Messenger = (RadioButton)findViewById(R.id.messenger);
@@ -50,30 +57,106 @@ public class MainActivity extends AppCompatActivity {
         speak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+
+                mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                         RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                //
+                mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS,20000000);
+
                 if(French.isChecked()) {
-                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "fr");
+                    mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "fr");
                 } else if (English.isChecked()) {
-                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+                    mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
                 } else if (Spanish.isChecked()) {
-                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "es");
+                    mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "es");
                 }
                 else {
-                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                    mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
                 }
 
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,
                         "Need to speak");
-                try {
-                    startActivityForResult(intent, REQ_CODE);
+
+                mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+                mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+                    @Override
+                    public void onReadyForSpeech(Bundle bundle) {
+                        Log.i("", "onReadyForSpeech");
+                    }
+
+                    @Override
+                    public void onBeginningOfSpeech() {
+                        Log.i("", "onBeginningOfSpeech");
+
+                    }
+
+                    @Override
+                    public void onRmsChanged(float v) {
+                        Log.i("", "onRmsChanged");
+
+                    }
+
+                    @Override
+                    public void onBufferReceived(byte[] bytes) {
+                        Log.i("", "onBufferReceived");
+
+                    }
+
+                    @Override
+                    public void onEndOfSpeech() {
+                        Log.i("", "onEndOfSpeech");
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+
+                    }
+
+                    public void onError(int errorCode)
+                    {
+                        Log.i("", "onError");
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                    }
+
+                    @Override
+                    public void onResults(Bundle bundle) {
+                        Log.i("", "onResults");
+                        ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                        String s = "";
+                        for (String result:matches)
+                            s += result + "\n";
+
+                        textToSend = textToSend + s;
+                        textWidget.setText(textToSend);
+                    }
+
+                    @Override
+                    public void onPartialResults(Bundle bundle) {
+                        Log.i("", "onPartialResults");
+
+                    }
+
+                    @Override
+                    public void onEvent(int i, Bundle bundle) {
+                        Log.i("", "onEvent");
+
+                    }
+                });
+                mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                /*try {
+                    startActivityForResult(mSpeechRecognizerIntent, REQ_CODE);
                 } catch (ActivityNotFoundException a) {
                     Toast.makeText(getApplicationContext(),
                             "Sorry your device not supported",
                             Toast.LENGTH_SHORT).show();
-                }
+                }*/
+            }
+        });
+
+        endRecording.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSpeechRecognizer.stopListening();
+                mSpeechRecognizer.destroy();
+                mSpeechRecognizer.cancel();
+                stopListering = true;
             }
         });
 
@@ -86,8 +169,11 @@ public class MainActivity extends AppCompatActivity {
                     sendWhatsApp();
                 } else if (Mail.isChecked()) {
                     sendEmail();
-                } else if (Google.isChecked()) {
+                } else if (Google.isChecked()) { // ACTION_WEB_SEARCH?
                     googleQuery();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Please select where to send this",Toast.LENGTH_SHORT);
                 }
             }
         });
@@ -96,33 +182,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 textWidget.setText("");
+                textToSend = "";
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case REQ_CODE: {
-                if (resultCode == RESULT_OK && null != data) {
-
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    textWidget.setText(result.get(0));
-                }
-                break;
-            }
-        }
-    }
-
     public void sendMessenger() {
         Intent sendIntent = new Intent();
+        textToSend = textWidget.getText().toString();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent
-                .putExtra(Intent.EXTRA_TEXT,
-                        textWidget.getText().toString());
+        sendIntent.putExtra(Intent.EXTRA_TEXT,textToSend);
         sendIntent.setType("text/plain");
         sendIntent.setPackage("com.facebook.orca");
         try {
@@ -140,14 +209,14 @@ public class MainActivity extends AppCompatActivity {
 
             Intent waIntent = new Intent(Intent.ACTION_SEND);
             waIntent.setType("text/plain");
-            String text = textWidget.getText().toString();
+            textToSend = textWidget.getText().toString();
 
             PackageInfo info=pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
             //Check if package exists or not. If not then code
             //in catch block will be called
             waIntent.setPackage("com.whatsapp");
 
-            waIntent.putExtra(Intent.EXTRA_TEXT, text);
+            waIntent.putExtra(Intent.EXTRA_TEXT, textToSend);
             startActivity(Intent.createChooser(waIntent, "Share with"));
 
         } catch (PackageManager.NameNotFoundException e) {
@@ -157,26 +226,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendEmail() {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("plain/text");
-        //intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "some@email.address" });
-        //intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
-        String text = textWidget.getText().toString();
-        intent.putExtra(Intent.EXTRA_TEXT, text);
-        startActivity(Intent.createChooser(intent, ""));
+        Intent mSpeechRecognizerIntent = new Intent(Intent.ACTION_SEND);
+        mSpeechRecognizerIntent.setType("plain/text");
+        //mSpeechRecognizerIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { "some@email.address" });
+        //mSpeechRecognizerIntent.putExtra(Intent.EXTRA_SUBJECT, "subject");
+        textToSend = textWidget.getText().toString();
+        mSpeechRecognizerIntent.putExtra(Intent.EXTRA_TEXT, textToSend);
+        startActivity(Intent.createChooser(mSpeechRecognizerIntent, ""));
     }
 
     public void googleQuery() {
-        String text = textWidget.getText().toString();
+        textToSend = textWidget.getText().toString();
         String escapedQuery = null;
         try {
-            escapedQuery = URLEncoder.encode(text, "UTF-8");
+            escapedQuery = URLEncoder.encode(textToSend, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         Uri uri = Uri.parse("http://www.google.com/#q=" + escapedQuery);
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
+        Intent mSpeechRecognizerIntent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(mSpeechRecognizerIntent);
     }
 
 }
